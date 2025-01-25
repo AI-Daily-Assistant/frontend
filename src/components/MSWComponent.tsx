75f17b81c3589e0bc5e44d0d28eb1cac0cd78921
eunwoo-levi <eunwoo1341@gmail.com>
2025-01-25 13:43:56 +0900

    feat: make MSWComponent work only in dev env

--- src/components/MSWComponent.tsx
+++ src/components/MSWComponent.tsx
@@ -2,26 +2,38 @@
 
 import { useEffect, useState } from 'react';
 
-export const MSWComponent = ({ children }: { children: React.ReactNode }) => {
+interface MSWComponentProps {
+  children: React.ReactNode;
+  loadingComponent?: React.ReactNode;
+}
+
+export const MSWComponent = ({
+  children,
+  loadingComponent,
+}: MSWComponentProps) => {
   const [mswReady, setMswReady] = useState(false);
 
   useEffect(() => {
     const init = async () => {
-      try {
-        const { initMsw } = await import('../../mocks/index');
-        await initMsw();
-        setMswReady(true);
-      } catch (error) {
-        console.error('MSW initialization failed:', error);
+      // 환경 변수 체크 (개발 환경에서만 실행)
+      if (process.env.NEXT_PUBLIC_MOCK_API === 'true') {
+        try {
+          const { initMsw } = await import('../../mocks/index');
+          await initMsw();
+          console.log('MSW initialized');
+        } catch (error) {
+          console.error('MSW initialization failed:', error);
+        }
+      } else {
+        console.log('MSW is disabled in this environment');
       }
+      setMswReady(true);
     };
 
-    if (!mswReady) {
-      init();
-    }
-  }, [mswReady]);
+    init();
+  }, []);
 
-  if (!mswReady) return <div>Loading...</div>;
+  if (!mswReady) return loadingComponent || <div>Loading mock server...</div>;
 
   return <>{children}</>;
 };
